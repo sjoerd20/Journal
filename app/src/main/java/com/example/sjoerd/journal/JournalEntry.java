68d0945479ae6050168498d25d40ec23b6eba19c
Sjoerd20 <sjoerd.terpstra@outlook.com>
2018-11-26 15:51:16 +0100

    added activities and JournalEntry class

---
+++ app/src/main/java/com/example/sjoerd/journal/JournalEntry.java
@@ -0,0 +1,58 @@
+package com.example.sjoerd.journal;
+
+import java.io.Serializable;
+
+public class JournalEntry implements Serializable {
+    private int id;
+    private String title, content, mood, timestamp;
+
+    // constructor
+    public JournalEntry(int id, String title, String content, String mood, String timestamp) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.mood = mood;
+        this.timestamp = timestamp;
+    }
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
--- app/src/main/java/com/example/sjoerd/journal/MainActivity.java
+++ app/src/main/java/com/example/sjoerd/journal/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sjoerd.journal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void floatingActionButtonClicked(View v) {
+        Intent intent = new Intent(MainActivity.this, InputActivity.class);
+        startActivity(intent);
+    }
+
+    private class OnItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            // TODO implement listener
+        }
+    }
+
+    private class OnItemLongClickListener implements AdapterView.OnItemLongClickListener {
+        @Override
+        public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+            // TODO implement listener
+            return true;
+        }
     }
 }
